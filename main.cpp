2567b856a6537197e27d29cda93c782bec9ed811
Marcos <marcos.lopezmendoza@gmail.com>
2013-11-09 12:30:59 -0600

    Creando main.cpp
    Aquí va estar la solución del proyecto

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Pila.h"
+#include "Cola.h"
+
+int main(int argc, const char * argv [])
+{
+  cout << "Esto es una prueba de que funcionaron los archivos\n";
+  //system("pause");
+  return 0;
+}
