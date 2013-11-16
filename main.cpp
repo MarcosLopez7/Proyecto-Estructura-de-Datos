6cf1b832b8070005e03b6ba79c9caac6d7318e62
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2013-11-16 12:09:20 -0600

    Priueba de los juegos

--- main.cpp
+++ main.cpp
@@ -1,14 +1,61 @@
-#include "Persona.h"
+#include "Atraccion.h"
+#include "Juego.h"
 #include <iostream>
 
 int main(int argc, const char * argv [])
 {
 
+
+
+	Juego * batman = new Juego(0, 0, "batman");
+
+	Cola<Persona *> * parque = new Cola<Persona *>();
+
 	for (int t = 600; t < 1200; t = t + 10)
 	{
-		Persona * personon = new Persona(t);
-		cout << *personon;
-		delete personon;
+
+		cout << "PRUEBA\n";
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			Persona * personon = new Persona(t);
+			Nodo<Persona *> * nodulon = new Nodo<Persona *>(personon);
+			parque->enqueue(nodulon);
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+
+			batman->EntraColaNormal(parque->dequeue()->getInfo());
+		}
+
+		batman->setTiempoI(t);
+		batman->setTiempoF(t + 5);
+
+		for (int i = batman->getTiempoI(); i <= batman->getTiempoF(); i++)
+		{
+			if (i == t)
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					batman->EntraJuego(batman->SalColaNormal());
+				}
+			}
+			if (i == (t + 5))
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					parque->enqueue(new Nodo<Persona *>(batman->SalJuego()));
+				}
+			}
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+			delete parque->dequeue();
+		}
+
 	}
 
 	system("pause");
