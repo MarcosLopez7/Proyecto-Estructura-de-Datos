52f539b052a02e24b4f8f6077a8cfeac58bb7f0b
Marcos <marcos.lopezmendoza@gmail.com>
2013-11-25 01:06:01 -0600

    Implementar el algoritmo de Dijkstra
    Me tardé un buen en entenderlo, pero creo que ya lo hice, lo malo es que mi computadora le sale errores que no entiendo porque salen, errores bien difíciles de comprender, así que a ver si funciona con otra compu...

--- Grafo.h
+++ Grafo.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Arista.h"
+#include <vector>
+#include <queue>
 
 template <class V, class A> class Grafo;
 template <typename V, typename A> std::ostream & operator <<(std::ostream & os, Grafo<V, A>  & g);
@@ -8,11 +10,19 @@ template <class V, class A>
 class Grafo
 {
 	Vertice<V, A> * inicio;
+	int cantidadNodo;
+
+	/*Estos atributos son para hacer dijkstra MAS NADA*/
+	vector<int> * distancia = new vector<int>();
+	vector<bool> * visitados = new vector<bool>();
+	priority_queue<Vertice<V, A> *, int> cola;
+	vector<int> * previo = new vector<int>();
 
 public:
 	Grafo()
 	{
 		inicio = NULL;
+		cantidadNodo = 0;
 	}
 
 	~Grafo();
@@ -25,6 +35,12 @@ public:
 	void busquedaArista(Arista<V, A> *);
 	//void eliminarArista(Arista<V, A> * );
 	//void eliminarVertice()
+	void iniciar();
+	int posicionDelNodo(Vertice<V, A> *);
+	int dijkstra(Vertice<V, A> *, Vertice<V, A> *);
+	//vector<int> dijkstra(Vertice<V, A> *);
+	void relajacion(Vertice<V, A> *, Vertice<V, A> *, int);
+	
 
 	friend std::ostream & operator << <>(std::ostream &, Grafo<V, A> &);
 };
@@ -76,6 +92,8 @@ void Grafo<V, A>::insertarVertice(Vertice<V, A> * v)
 
 		temp->setNext(v);
 	}
+
+	cantidadNodo++;
 }
 
 template <class V, class A>
@@ -136,21 +154,21 @@ int Grafo<V, A>::entradas(Vertice<V, A> * v)
 void Grafo<V, A>::eliminarArista(Arista<V, A> * a)
 {
 
-	Vertice<V, A> * temp = inicio;
-	
+Vertice<V, A> * temp = inicio;
 
-	while (temp != NULL)
-	{
 
-		Arista<V, A> * tempA = temp->getAristas();
-		while (tempA != NULL && tempA != a)
-		{
-			tempA = tempA->getNext();
-		}
-		temp = temp->getNext();
-	}
+while (temp != NULL)
+{
 
-	tempA = 
+Arista<V, A> * tempA = temp->getAristas();
+while (tempA != NULL && tempA != a)
+{
+tempA = tempA->getNext();
+}
+temp = temp->getNext();
+}
+
+tempA =
 
 }*/
 
@@ -165,5 +183,78 @@ void Grafo<V, A>::busquedaVertice(Vertice<V, A> * v)
 template<class V, class A>
 void Grafo<V, A>::busquedaArista(Arista<V, A> * a)
 {
+
+}
+
+template<class V, class A>
+void Grafo<V, A>::iniciar()
+{
+	for (int i = 0; i <= cantidadNodo; i++)
+	{
+		distancia[i] = 100000;
+		visitados[i] = true;
+		previo[i] = -1;
+	}
+}
+
+template<class V, class A>
+void Grafo<V, A>::relajacion(Vertice<V, A> * ac, Vertice<V, A> * ad, int p)
+{
+
+	if (distancia[posicionDelNodo(ac)] + p < distancia[posicionDelNodo(ad)])
+	{
+		distancia[posicionDelNodo(ad)] = distancia[posicionDelNodo(ac)] + p;
+		//previo [posicionDelNodo(ad)] =
+		cola.push(ad, distancia[posicionDelNodo(ad)])
+	}
+
+}
+
+template<class V, class A>
+int Grafo<V, A>::dijkstra(Vertice<V, A> * i, Vertice<V, A> * d)
+{
+	iniciar();
+	cola.push(i, 0);
+	distancia[posicionDelNodo(i)] = 0;
+	Vertice<V, A> * actual;
+	Vertice<V, A> * adyacente;
+	int peso;
+	while (!cola.empty())
+	{
+		actual = cola.top();
+		cola.pop();
+		if (visitados[posicionDelNodo(actual)])
+			continue;
+		visitados[posicionDelNodo(actual)] = true;
+
+		for (int i = 0; i < actual->numeroAristas(); i++)
+		{
+			adyacente = actual->getAristas()->getDestino();
+			peso = adyacente->getAristas()->getPeso();
+			if (!visitados[posicionDelNodo(adyacente)])
+			{
+				relajacion(actual, adyacente, peso);
+			}
+		}
+	}
+
+	return distancia[posicionDelNodo(d)];
+
+}
+
+template<class V, class A>
+int Grafo<V, A>::posicionDelNodo(Vertice<V, A> * v)
+{
+
+	Vertice<V, A> * temp = inicio;
+	int cont = 0;
+
+	while (temp != NULL && temp == v)
+	{
+		cont++;
+		temp = temp->getNext();
+	}
+
+	return cont;
 
 }
